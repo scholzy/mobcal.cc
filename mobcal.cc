56dd0dc8ffc046a3df49a688048a3c425f59e035
Michael Scholz <m@scholz.moe>
2018-06-19 10:06:22 +1000

    Standardize formatting with clang-format

--- mobcal.cc
+++ mobcal.cc
@@ -12,258 +12,250 @@
 
 Molecule get_input(std::string file)
 {
-	std::fstream input_file(file.c_str());
-	int elem;
-	double x, y, z, charge;
-	Molecule molecule;
-	
-	while (input_file >> elem >> x >> y >> z >> charge) {
-		Atom a = { elem,
-			   charge,
-			   x,
-			   y,
-			   z };
-		molecule.push_back(a);
-	}
+    std::fstream input_file(file.c_str());
+    int elem;
+    double x, y, z, charge;
+    Molecule molecule;
 
-	return molecule;
+    while (input_file >> elem >> x >> y >> z >> charge) {
+        Atom a = { elem,
+            charge,
+            x,
+            y,
+            z };
+        molecule.push_back(a);
+    }
+
+    return molecule;
 }
 
 Point get_extents(Molecule* molecule, double dmax)
 {
-	int irn = 1000;
-	double ddd = (ROMAX + dmax) / (double)irn;
-	Point e_max   = { 0.0, 0.0, 0.0 };
-	Point r_max   = { 0.0, 0.0, 0.0 };
-	Point r00_max = { 0.0, 0.0, 0.0 };
-	Point coord   = { 0.0, 0.0, 0.0 };
+    int irn = 1000;
+    double ddd = (ROMAX + dmax) / (double)irn;
+    Point e_max = { 0.0, 0.0, 0.0 };
+    Point r_max = { 0.0, 0.0, 0.0 };
+    Point r00_max = { 0.0, 0.0, 0.0 };
+    Point coord = { 0.0, 0.0, 0.0 };
 
-	for (int i = 0; i < irn; ++i) {
-		coord.x = ROMAX + dmax - ((double)i + 1.0) * ddd;
-		Potential p = potential(molecule, coord);
-		if (p.potential > 0.0) {
-			break;
-		}
-		r00_max.x = coord.x;
-		if (p.potential < e_max.x) {
-			e_max.x = p.potential;
-			r_max.x = coord.x;
-		}
-	}
-	coord.x = 0.0;
+    for (int i = 0; i < irn; ++i) {
+        coord.x = ROMAX + dmax - ((double)i + 1.0) * ddd;
+        Potential p = potential(molecule, coord);
+        if (p.potential > 0.0) {
+            break;
+        }
+        r00_max.x = coord.x;
+        if (p.potential < e_max.x) {
+            e_max.x = p.potential;
+            r_max.x = coord.x;
+        }
+    }
+    coord.x = 0.0;
 
-	for (int i = 0; i < irn; ++i) {
-		coord.y = ROMAX + dmax - ((double)i + 1.0) * ddd;
-		Potential p = potential(molecule, coord);
-		if (p.potential > 0.0) {
-			break;
-		}
-		r00_max.y = coord.y;
-		if (p.potential < e_max.y) {
-			e_max.y = p.potential;
-			r_max.y = coord.y;
-		}
-	}
-	coord.y = 0.0;
+    for (int i = 0; i < irn; ++i) {
+        coord.y = ROMAX + dmax - ((double)i + 1.0) * ddd;
+        Potential p = potential(molecule, coord);
+        if (p.potential > 0.0) {
+            break;
+        }
+        r00_max.y = coord.y;
+        if (p.potential < e_max.y) {
+            e_max.y = p.potential;
+            r_max.y = coord.y;
+        }
+    }
+    coord.y = 0.0;
 
-	for (int i = 0; i < irn; ++i) {
-		coord.z = ROMAX + dmax - ((double)i + 1.0) * ddd;
-		Potential p = potential(molecule, coord);
-		if (p.potential > 0.0) {
-			break;
-		}
-		r00_max.z = coord.z;
-		if (p.potential < e_max.z) {
-			e_max.z = p.potential;
-			r_max.z = coord.z;
-		}
-	}
+    for (int i = 0; i < irn; ++i) {
+        coord.z = ROMAX + dmax - ((double)i + 1.0) * ddd;
+        Potential p = potential(molecule, coord);
+        if (p.potential > 0.0) {
+            break;
+        }
+        r00_max.z = coord.z;
+        if (p.potential < e_max.z) {
+            e_max.z = p.potential;
+            r_max.z = coord.z;
+        }
+    }
 
-	std::cout << "Well depths along the x-, y-, and z-axes:" << std::endl;
-	std::cout << e_max.x / XE << "\t"
-		  << e_max.y / XE << "\t"
-		  << e_max.z / XE << "\n" << std::endl;
+    std::cout << "Well depths along the x-, y-, and z-axes:" << std::endl;
+    std::cout << e_max.x / XE << "\t"
+              << e_max.y / XE << "\t"
+              << e_max.z / XE << "\n"
+              << std::endl;
 
-	std::cout << "Well coordinates along the x-, y-, and z-axes:" << std::endl;
-	std::cout << r_max.x / 1e-10 << "\t"
-		  << r_max.y / 1e-10 << "\t"
-		  << r_max.z / 1e-10 << "\n" << std::endl;
+    std::cout << "Well coordinates along the x-, y-, and z-axes:" << std::endl;
+    std::cout << r_max.x / 1e-10 << "\t"
+              << r_max.y / 1e-10 << "\t"
+              << r_max.z / 1e-10 << "\n"
+              << std::endl;
 
-	return r_max;
+    return r_max;
 }
 
 void setup_gst(Molecule* molecule, double* wgst, double* pgst)
 {
-	double dgst = 5.0e-7 * 6.0 * sqrt(TST);
-	double gst = dgst;
-	double sum = 0.0, sum1 = 0.0, sum2 = 0.0;
-	double hold1 = 0.0, hold2 = 0.0, hold3 = 0.0;
+    double dgst = 5.0e-7 * 6.0 * sqrt(TST);
+    double gst = dgst;
+    double sum = 0.0, sum1 = 0.0, sum2 = 0.0;
+    double hold1 = 0.0, hold2 = 0.0, hold3 = 0.0;
 
-	for (int i = 0; i < INP; ++i ) {
-		sum1 += sqrt((double)i + 1.0);
-	}
+    for (int i = 0; i < INP; ++i) {
+        sum1 += sqrt((double)i + 1.0);
+    }
 
-	for (int i = 0; i < INP; ++i) {
-		hold1 = sqrt((double)i + 1.0);
-		hold2 = sqrt((double)i);
-		sum2 += hold2;
-		wgst[i] = hold1 / sum1;
-		double gstt = std::pow(TST, 3) * (sum2 + (hold1 / 2.0)) / sum1;
+    for (int i = 0; i < INP; ++i) {
+        hold1 = sqrt((double)i + 1.0);
+        hold2 = sqrt((double)i);
+        sum2 += hold2;
+        wgst[i] = hold1 / sum1;
+        double gstt = std::pow(TST, 3) * (sum2 + (hold1 / 2.0)) / sum1;
 
-		do {
-			sum += std::exp(-1.0 * std::pow(gst, 2) / TST) *
-				std::pow(gst, 5) * dgst;
-			gst += dgst;
-			if (sum > gstt) {
-				pgst[i] = gst - (dgst / 2.0);
-			}
-		} while (sum < gstt);
+        do {
+            sum += std::exp(-1.0 * std::pow(gst, 2) / TST) * std::pow(gst, 5) * dgst;
+            gst += dgst;
+            if (sum > gstt) {
+                pgst[i] = gst - (dgst / 2.0);
+            }
+        } while (sum < gstt);
 
-		hold1 = std::sqrt((pgst[i] * pgst[i] * EO) /
-				  (0.5 * mu(molecule)));
-		hold2 = 0.5 * mu(molecule) * std::pow(hold1, 2) / (XK * T);
-		hold3 = std::exp(-1.0 * std::pow(pgst[i], 2) / TST) *
-			std::pow(pgst[i], 5);
-	}
+        hold1 = std::sqrt((pgst[i] * pgst[i] * EO) / (0.5 * mu(molecule)));
+        hold2 = 0.5 * mu(molecule) * std::pow(hold1, 2) / (XK * T);
+        hold3 = std::exp(-1.0 * std::pow(pgst[i], 2) / TST) * std::pow(pgst[i], 5);
+    }
 }
-
 
 void setup_bst(Molecule* molecule, double* wgst, double* pgst, double* b2max, double* cosx, Point extents)
 {
-	double dbst2 = 1.0;
-	double dbst22 = dbst2 / 10.0;
+    double dbst2 = 1.0;
+    double dbst22 = dbst2 / 10.0;
 
-	for (int i = 0; i < INP; i++) {
-		b2max[i] = 0.0;
-	}
+    for (int i = 0; i < INP; i++) {
+        b2max[i] = 0.0;
+    }
 
-	for (int i = INP - 1; i >= 0; i--) {
-		double gst2 = std::pow(pgst[i], 2);
-		double v = std::sqrt((gst2 * EO) / (0.5 * mu(molecule)));
-		int ibst = std::trunc(extents.x / RO) - 6;
-		
-		if (i + 1 < INP) {
-			ibst = std::trunc(b2max[i + 1] / dbst2) - 6;
-		}
+    for (int i = INP - 1; i >= 0; i--) {
+        double gst2 = std::pow(pgst[i], 2);
+        double v = std::sqrt((gst2 * EO) / (0.5 * mu(molecule)));
+        int ibst = std::trunc(extents.x / RO) - 6;
 
-		if (ibst < 0) {
-			ibst = 0;
-		}
+        if (i + 1 < INP) {
+            ibst = std::trunc(b2max[i + 1] / dbst2) - 6;
+        }
 
-		double bst2 = 0.0, b = 0.0;
-		do {
-			bst2 = dbst2 * ((double)ibst);
-			b = RO * std::sqrt(bst2);
-			Trajout trj = trajectory(molecule, v, b);
-			cosx[ibst] = 1.0 - std::cos(trj.ang);
+        if (ibst < 0) {
+            ibst = 0;
+        }
 
-			if (ibst > 5) {
-				if ((cosx[ibst] < CMIN) &&
-				    (cosx[ibst - 1] < CMIN) &&
-				    (cosx[ibst - 2] < CMIN) &&
-				    (cosx[ibst - 3] < CMIN) &&
-				    (cosx[ibst - 4] < CMIN)) {
-					break;
-				}
-			}
-			ibst += 1;
-		} while (1);
+        double bst2 = 0.0, b = 0.0;
+        do {
+            bst2 = dbst2 * ((double)ibst);
+            b = RO * std::sqrt(bst2);
+            Trajout trj = trajectory(molecule, v, b);
+            cosx[ibst] = 1.0 - std::cos(trj.ang);
 
-		b2max[i] = (double)(ibst - 5) * dbst2;
-		Trajout trj;
-		do {
-			b2max[i] += dbst22;
-			double b = RO * std::sqrt(b2max[i]);
-			trj = trajectory(molecule, v, b);
-		} while (1.0 - std::cos(trj.ang) > CMIN);
-	}
+            if (ibst > 5) {
+                if ((cosx[ibst] < CMIN) && (cosx[ibst - 1] < CMIN) && (cosx[ibst - 2] < CMIN) && (cosx[ibst - 3] < CMIN) && (cosx[ibst - 4] < CMIN)) {
+                    break;
+                }
+            }
+            ibst += 1;
+        } while (1);
+
+        b2max[i] = (double)(ibst - 5) * dbst2;
+        Trajout trj;
+        do {
+            b2max[i] += dbst22;
+            double b = RO * std::sqrt(b2max[i]);
+            trj = trajectory(molecule, v, b);
+        } while (1.0 - std::cos(trj.ang) > CMIN);
+    }
 }
 
 void calculate(Molecule* molecule, double* wgst, double* pgst, double* b2max, double* cosx, std::mt19937_64 rng)
 {
-	double q1st[INP] = { 0.0 };
-	double q2st[INP] = { 0.0 };
+    double q1st[INP] = { 0.0 };
+    double q2st[INP] = { 0.0 };
 
-	double om11st[ITN] = { 0.0 };
-	double om12st[ITN] = { 0.0 };
-	double om13st[ITN] = { 0.0 };
-	double om22st[ITN] = { 0.0 };
+    double om11st[ITN] = { 0.0 };
+    double om12st[ITN] = { 0.0 };
+    double om13st[ITN] = { 0.0 };
+    double om22st[ITN] = { 0.0 };
 
-	std::uniform_real_distribution<double> unif;
-	for (int i = 0; i < ITN; ++i) {
-		om11st[i] = 0.0;
-		om12st[i] = 0.0;
-		om13st[i] = 0.0;
-		om22st[i] = 0.0;
+    std::uniform_real_distribution<double> unif;
+    for (int i = 0; i < ITN; ++i) {
+        om11st[i] = 0.0;
+        om12st[i] = 0.0;
+        om13st[i] = 0.0;
+        om22st[i] = 0.0;
 
-		for (int j = 0; j < INP; ++j) {
-			double gst2 = std::pow(pgst[j], 2);
-			double v = std::sqrt((gst2 * EO) / (0.5 * mu(molecule)));
-			double temp1 = 0.0, temp2 = 0.0;
+        for (int j = 0; j < INP; ++j) {
+            double gst2 = std::pow(pgst[j], 2);
+            double v = std::sqrt((gst2 * EO) / (0.5 * mu(molecule)));
+            double temp1 = 0.0, temp2 = 0.0;
 
 #pragma omp parallel for // reduction(+:temp1,temp2)
-			for (int k = 0; k < IMP; ++k) {
-				double rnb = unif(rng);
-				double rands[3] = { unif(rng), unif(rng), unif(rng) };
-				Molecule mol = rantate(molecule, rands);
-				double bst2 = rnb * b2max[j];
-				double b = RO * std::sqrt(bst2);
-				Trajout trj = trajectory(&mol, v, b);
-				double hold1 = 1.0 - std::cos(trj.ang);
-				double hold2 = std::pow(std::sin(trj.ang), 2);
-				temp1 += hold1 * b2max[j] / (double)IMP;
-				temp2 += 1.5 * hold2 * b2max[j] / (double)IMP;
-			}
+            for (int k = 0; k < IMP; ++k) {
+                double rnb = unif(rng);
+                double rands[3] = { unif(rng), unif(rng), unif(rng) };
+                Molecule mol = rantate(molecule, rands);
+                double bst2 = rnb * b2max[j];
+                double b = RO * std::sqrt(bst2);
+                Trajout trj = trajectory(&mol, v, b);
+                double hold1 = 1.0 - std::cos(trj.ang);
+                double hold2 = std::pow(std::sin(trj.ang), 2);
+                temp1 += hold1 * b2max[j] / (double)IMP;
+                temp2 += 1.5 * hold2 * b2max[j] / (double)IMP;
+            }
 
-			om11st[i] += temp1 * wgst[j];
-			om12st[i] += temp1 * pgst[j] * pgst[j] * wgst[j] * (1.0 / (3.0 * TST));
-			om13st[i] += temp1 * pow(pgst[j], 4) * wgst[j] * (1.0 / (12.0 * TST * TST));
-			om22st[i] += temp2 * pgst[j] * pgst[j] * wgst[j] / (1.0 / (3.0 * TST));
-			q1st[j] += temp1;
-			q2st[j] += temp2;
-		}
+            om11st[i] += temp1 * wgst[j];
+            om12st[i] += temp1 * pgst[j] * pgst[j] * wgst[j] * (1.0 / (3.0 * TST));
+            om13st[i] += temp1 * pow(pgst[j], 4) * wgst[j] * (1.0 / (12.0 * TST * TST));
+            om22st[i] += temp2 * pgst[j] * pgst[j] * wgst[j] / (1.0 / (3.0 * TST));
+            q1st[j] += temp1;
+            q2st[j] += temp2;
+        }
 
-		printf("Cycle %2d: %f\n", i, om11st[i] * M_PI * RO * RO * 1e20);
-	}
+        printf("Cycle %2d: %f\n", i, om11st[i] * M_PI * RO * RO * 1e20);
+    }
 
-	double sum = 0.0;
-	for (int i = 0; i < ITN; i++) {
-		sum += om11st[i] * M_PI * RO * RO * 1e20;
-	}
-	sum /= (double)ITN;
-	printf("Average CCS: %f\n", sum);
+    double sum = 0.0;
+    for (int i = 0; i < ITN; i++) {
+        sum += om11st[i] * M_PI * RO * RO * 1e20;
+    }
+    sum /= (double)ITN;
+    printf("Average CCS: %f\n", sum);
 }
 
 void mobil2(Molecule molecule)
 {
-	const unsigned int seed = time(0);
-	std::mt19937_64 rng(seed);
+    const unsigned int seed = time(0);
+    std::mt19937_64 rng(seed);
 
-	align_com(&molecule);
+    align_com(&molecule);
 
-	double distance_max = align_x(&molecule);
+    double distance_max = align_x(&molecule);
 
-	Point extents = get_extents(&molecule, distance_max);
+    Point extents = get_extents(&molecule, distance_max);
 
-	double wgst[INP];
-	double pgst[INP];
-	setup_gst(&molecule, wgst, pgst);
+    double wgst[INP];
+    double pgst[INP];
+    setup_gst(&molecule, wgst, pgst);
 
-	double b2max[INP];
-	double cosx[2000];
-	setup_bst(&molecule, wgst, pgst, b2max, cosx, extents);
+    double b2max[INP];
+    double cosx[2000];
+    setup_bst(&molecule, wgst, pgst, b2max, cosx, extents);
 
-	calculate(&molecule, wgst, pgst, b2max, cosx, rng);
-
-
+    calculate(&molecule, wgst, pgst, b2max, cosx, rng);
 }
 
 int main(int argc, char** argv)
 {
-	auto input = (std::string)argv[1];
-	Molecule molecule = get_input(input);
+    auto input = (std::string)argv[1];
+    Molecule molecule = get_input(input);
 
-	mobil2(molecule);
+    mobil2(molecule);
 
-	return 0;
+    return 0;
 }
--- trajectory.cc
+++ trajectory.cc
@@ -2,239 +2,233 @@
 
 void derivative(Molecule* molecule, double* w, double* dw)
 {
-	dw[0] = w[1] / mu(molecule);
-	dw[2] = w[3] / mu(molecule);
-	dw[4] = w[5] / mu(molecule);
+    dw[0] = w[1] / mu(molecule);
+    dw[2] = w[3] / mu(molecule);
+    dw[4] = w[5] / mu(molecule);
 
-	Point point = { w[0], w[2], w[4] };
-	Potential p = potential(molecule, point);
-	dw[1] = -1.0 * p.force.x;
-	dw[3] = -1.0 * p.force.y;
-	dw[5] = -1.0 * p.force.z;
+    Point point = { w[0], w[2], w[4] };
+    Potential p = potential(molecule, point);
+    dw[1] = -1.0 * p.force.x;
+    dw[3] = -1.0 * p.force.y;
+    dw[5] = -1.0 * p.force.z;
 }
 
 void diffeq(Molecule* molecule, double* w, double* dw, double* dt, int* l, double* q, double* hvar, double* hcvar, double* time, double array[][40])
 {
-	double a[4] = { 0.50, 0.292893218814, 1.70710678118, 0.1666666666667 };
-	double b[4] = { 2.0, 1.0, 1.0, 2.0 };
-	double c[4] = { -0.5, -0.292893218814, -1.70710678118, -0.5 };
+    double a[4] = { 0.50, 0.292893218814, 1.70710678118, 0.1666666666667 };
+    double b[4] = { 2.0, 1.0, 1.0, 2.0 };
+    double c[4] = { -0.5, -0.292893218814, -1.70710678118, -0.5 };
 
-	double ampc[5] = { -0.111059153612,
-			   0.672667757774,
-			   -1.70633621697,
-			   2.33387888707,
-			   -1.8524668225 };
-	double amcc[4] = {  0.0189208128941,
-			    -0.121233356692,
-			    0.337771548703,
-			    -0.55921513665 };
-	double var = 2.97013888888;
-	double cvar = 0.990972222222;
-	double acst = 0.332866152768;
-	
-	if (*l == 0) {
-		for (int j = 0; j < 6; ++j) {
-			q[j] = 0.0;
-		}
-		*hvar = (*dt) * var;
-		*hcvar = (*dt) * cvar;
-	}
+    double ampc[5] = { -0.111059153612,
+        0.672667757774,
+        -1.70633621697,
+        2.33387888707,
+        -1.8524668225 };
+    double amcc[4] = { 0.0189208128941,
+        -0.121233356692,
+        0.337771548703,
+        -0.55921513665 };
+    double var = 2.97013888888;
+    double cvar = 0.990972222222;
+    double acst = 0.332866152768;
 
-	if (*l >= 0) {
-		*l += 1;
-		int k = 0;
-		do {
-			for (int j = 0; j < 4; j++) {
-				if (std::pow(-1, j + 1) > 0) {
-					*time += 0.5 * (*dt);
-				}
-				for (int i = 0; i < 6; i++) {
-					dw[i] *= (*dt);
-					double r = a[j] * (dw[i] - b[j] * q[i]);
-					// std::cout << r << std::endl;
-					w[i] += r;
-					q[i] += 3.0 * r + c[j] * dw[i];
-				}
-			}
+    if (*l == 0) {
+        for (int j = 0; j < 6; ++j) {
+            q[j] = 0.0;
+        }
+        *hvar = (*dt) * var;
+        *hcvar = (*dt) * cvar;
+    }
 
-			derivative(molecule, w, dw);
-			k += 1;
-		} while (k != 1);
+    if (*l >= 0) {
+        *l += 1;
+        int k = 0;
+        do {
+            for (int j = 0; j < 4; j++) {
+                if (std::pow(-1, j + 1) > 0) {
+                    *time += 0.5 * (*dt);
+                }
+                for (int i = 0; i < 6; i++) {
+                    dw[i] *= (*dt);
+                    double r = a[j] * (dw[i] - b[j] * q[i]);
+                    // std::cout << r << std::endl;
+                    w[i] += r;
+                    q[i] += 3.0 * r + c[j] * dw[i];
+                }
+            }
 
-		if ((*l) - 6 >= 0) {
-			*l = -1;
-			*dt *= 2.0;
-			return;
-		} else {
-			for (int j = 0; j < 6; j++) {
-				array[(*l) - 1][j] = dw[j];
-			}
-			return;
-		}
-	} else {
-		double savw[6] = { w[0], w[1], w[2], w[3], w[4], w[5] };
-		double savdw[6] = { dw[0], dw[1], dw[2], dw[3], dw[4], dw[5] };
-		for (int j = 0; j < 6; j++) {
-			array[5][j] = savdw[j];
-			for (int i = 0; i < 5; i++) {
-				array[5][j] += ampc[i] * array[i][j];
-			}
-			w[j] += array[5][j] * (*hvar);
-		}
-		*time += *dt;
-		derivative(molecule, w, dw);
-		for (int j = 0; j < 6; j++) {
-			array[5][j] = acst * dw[j];
-			for (int i = 0; i < 4; i++) {
-				array[i][j] = array[i + 1][j];
-				array[5][j] += array[i][j] * amcc[i];
-			}
-			array[4][j] = savdw[j];
-			w[j] = savw[j] + (*hcvar) * (array[4][j] + array[5][j]);
-		}
-		derivative(molecule, w, dw);
-	}
+            derivative(molecule, w, dw);
+            k += 1;
+        } while (k != 1);
+
+        if ((*l) - 6 >= 0) {
+            *l = -1;
+            *dt *= 2.0;
+            return;
+        } else {
+            for (int j = 0; j < 6; j++) {
+                array[(*l) - 1][j] = dw[j];
+            }
+            return;
+        }
+    } else {
+        double savw[6] = { w[0], w[1], w[2], w[3], w[4], w[5] };
+        double savdw[6] = { dw[0], dw[1], dw[2], dw[3], dw[4], dw[5] };
+        for (int j = 0; j < 6; j++) {
+            array[5][j] = savdw[j];
+            for (int i = 0; i < 5; i++) {
+                array[5][j] += ampc[i] * array[i][j];
+            }
+            w[j] += array[5][j] * (*hvar);
+        }
+        *time += *dt;
+        derivative(molecule, w, dw);
+        for (int j = 0; j < 6; j++) {
+            array[5][j] = acst * dw[j];
+            for (int i = 0; i < 4; i++) {
+                array[i][j] = array[i + 1][j];
+                array[5][j] += array[i][j] * amcc[i];
+            }
+            array[4][j] = savdw[j];
+            w[j] = savw[j] + (*hcvar) * (array[4][j] + array[5][j]);
+        }
+        derivative(molecule, w, dw);
+    }
 }
 
 Trajout trajectory(Molecule* molecule, double v, double b)
 {
-	Point vel = { 0.0, -1.0 * v, 0.0 };
+    Point vel = { 0.0, -1.0 * v, 0.0 };
 
-	/* Determine time step for integration */
-	double top = (v / 95.2381) - 0.5;
-	if (v >= 1000.0) {
-		top = 10.0;
-	} else if (v >= 2000) {
-		top = 10.0 - ((v - 2000.0) * 7.5e-3);
-	} else if (v >= 3000.0) {
-		top = 2.5;
-	}
+    /* Determine time step for integration */
+    double top = (v / 95.2381) - 0.5;
+    if (v >= 1000.0) {
+        top = 10.0;
+    } else if (v >= 2000) {
+        top = 10.0 - ((v - 2000.0) * 7.5e-3);
+    } else if (v >= 3000.0) {
+        top = 2.5;
+    }
 
-	double dt1 = top * DTSF1 * 1.0e-11 / v;
-	double dt2 = dt1 * DTSF2;
+    double dt1 = top * DTSF1 * 1.0e-11 / v;
+    double dt2 = dt1 * DTSF2;
 
-	double e0 = 0.5 * mu(molecule) * std::pow(v, 2);
-	Point coord = { b, 0.0, 0.0 };
+    double e0 = 0.5 * mu(molecule) * std::pow(v, 2);
+    Point coord = { b, 0.0, 0.0 };
 
-	double y_max = 0.0;
-	double y_min = 0.0;
-	for (auto atom: *molecule) {
-		if (atom.y > y_max) {
-			y_max = atom.y;
-		}
-		if (atom.y < y_min) {
-			y_min = atom.y;
-		}
-	}
-	y_max += 1e-10;
-	y_min -= 1e-10;
+    double y_max = 0.0;
+    double y_min = 0.0;
+    for (auto atom : *molecule) {
+        if (atom.y > y_max) {
+            y_max = atom.y;
+        }
+        if (atom.y < y_min) {
+            y_min = atom.y;
+        }
+    }
+    y_max += 1e-10;
+    y_min -= 1e-10;
 
-	coord.y = y_max;
-	Potential p = potential(molecule, coord);
+    coord.y = y_max;
+    Potential p = potential(molecule, coord);
 
-	if (std::fabs(p.potential / e0) <= SW1) {
-		do {
-			coord.y -= 1e-10;
-			p = potential(molecule, coord);
-		} while (std::fabs(p.potential) / e0 < SW1);
-	} else {
-		do {
-			coord.y += 10e-10;
-			p = potential(molecule, coord);
-		} while (std::fabs(p.potential) / e0 > SW1);
-		do {
-			coord.y -= 1e-10;
-			p = potential(molecule, coord);
-		} while (std::fabs(p.potential) / e0 > SW1);
-	}
+    if (std::fabs(p.potential / e0) <= SW1) {
+        do {
+            coord.y -= 1e-10;
+            p = potential(molecule, coord);
+        } while (std::fabs(p.potential) / e0 < SW1);
+    } else {
+        do {
+            coord.y += 10e-10;
+            p = potential(molecule, coord);
+        } while (std::fabs(p.potential) / e0 > SW1);
+        do {
+            coord.y -= 1e-10;
+            p = potential(molecule, coord);
+        } while (std::fabs(p.potential) / e0 > SW1);
+    }
 
-	double e_total = e0 + p.potential;
+    double e_total = e0 + p.potential;
 
-	/* Trajectory is ready to go! */
-	
-	double w[6] = { coord.x, vel.x * mu(molecule),
-			coord.y, vel.y * mu(molecule),
-			coord.z, vel.z * mu(molecule) };
-	double dw[6] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+    /* Trajectory is ready to go! */
 
-	derivative(molecule, w, dw);
+    double w[6] = { coord.x, vel.x * mu(molecule),
+        coord.y, vel.y * mu(molecule),
+        coord.z, vel.z * mu(molecule) };
+    double dw[6] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 
-	int l = 0;
-	int ns = 0;
-	int nw = 0;
-	double dt = dt1;
-	double hvar = 0.0;
-	double hcvar = 0.0;
-	double q[6] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
-	double time = 0.0;
-	double array[6][40];
-	
-	do {
-		do {
-			diffeq(molecule, w, dw, &dt, &l, q, &hvar, &hcvar, &time, array);
-			// std::cout << w[0] << " " << w[2] << " " << w[4] << std::endl;
-			nw += 1;
-		} while (nw != INWR);
+    derivative(molecule, w, dw);
 
-		ns += nw;
-		nw = 0;
+    int l = 0;
+    int ns = 0;
+    int nw = 0;
+    double dt = dt1;
+    double hvar = 0.0;
+    double hcvar = 0.0;
+    double q[6] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+    double time = 0.0;
+    double array[6][40];
 
-		if (ns > 30000) {
-			break;
-		}
+    do {
+        do {
+            diffeq(molecule, w, dw, &dt, &l, q, &hvar, &hcvar, &time, array);
+            // std::cout << w[0] << " " << w[2] << " " << w[4] << std::endl;
+            nw += 1;
+        } while (nw != INWR);
 
-		Point point = { w[0],
-				w[2],
-				w[4] };
-		p = potential(molecule, point);
+        ns += nw;
+        nw = 0;
 
-		if (p.dmax < ROMAX) {
-			continue;
-		}
+        if (ns > 30000) {
+            break;
+        }
 
-		if ((std::fabs(p.potential) / e0 > SW2) && (dt == dt1)) {
-			dt = dt2;
-			l = 0;
-		}
+        Point point = { w[0],
+            w[2],
+            w[4] };
+        p = potential(molecule, point);
 
-		if ((std::fabs(p.potential) / e0 < SW2) && (dt == dt2)) {
-			dt = dt1;
-			l = 0;
-		}
+        if (p.dmax < ROMAX) {
+            continue;
+        }
 
-		if (std::fabs(p.potential) / e0 > SW1) {
-			continue;
-		}
+        if ((std::fabs(p.potential) / e0 > SW2) && (dt == dt1)) {
+            dt = dt2;
+            l = 0;
+        }
 
-		if (ns < 50) {
-			continue;
-		}
+        if ((std::fabs(p.potential) / e0 < SW2) && (dt == dt2)) {
+            dt = dt1;
+            l = 0;
+        }
 
-		break;
-	} while (1);
+        if (std::fabs(p.potential) / e0 > SW1) {
+            continue;
+        }
 
-	double num = 0.0;
-	double den = 0.0;
-	double ang = 0.0;
-	
-	if (dw[0] >= 0) {
-		num = dw[2] * -1.0 * v;
-		den = v * std::sqrt(std::pow(dw[0], 2) +
-				    std::pow(dw[2], 2) +
-				    std::pow(dw[4], 2));
-		ang = std::acos(num / den);
-	} else {
-		num = dw[2] * -1.0 * v;
-		den = v * std::sqrt(std::pow(dw[0], 2) +
-				    std::pow(dw[2], 2) +
-				    std::pow(dw[4], 2));
-		ang = -1.0 * std::acos(num / den);
-	}
+        if (ns < 50) {
+            continue;
+        }
 
-	double e = 0.5 * mu(molecule) * (std::pow(dw[0], 2) +
-					 std::pow(dw[2], 2) +
-					 std::pow(dw[4], 2));
-	Trajout output = { ang,
-			   (e + p.potential) / e_total };
-	return output;
+        break;
+    } while (1);
+
+    double num = 0.0;
+    double den = 0.0;
+    double ang = 0.0;
+
+    if (dw[0] >= 0) {
+        num = dw[2] * -1.0 * v;
+        den = v * std::sqrt(std::pow(dw[0], 2) + std::pow(dw[2], 2) + std::pow(dw[4], 2));
+        ang = std::acos(num / den);
+    } else {
+        num = dw[2] * -1.0 * v;
+        den = v * std::sqrt(std::pow(dw[0], 2) + std::pow(dw[2], 2) + std::pow(dw[4], 2));
+        ang = -1.0 * std::acos(num / den);
+    }
+
+    double e = 0.5 * mu(molecule) * (std::pow(dw[0], 2) + std::pow(dw[2], 2) + std::pow(dw[4], 2));
+    Trajout output = { ang,
+        (e + p.potential) / e_total };
+    return output;
 }
